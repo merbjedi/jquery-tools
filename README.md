a460ff8e03a5f8528994afc2cb6bd34c89937b26
Jacques Crocker <merbjedi@gmail.com>
2009-11-15 03:06:43 -0800

    Reorganizing, with README

---
+++ README.md
@@ -0,0 +1,25 @@
+# JQuery Tools #
+
+JQuery Tools is an attractive set of jQuery plugins for many common tasks. It's extremely well documented, and has some great functionality out of the box.
+
+Learn more about JQuery Tools here:
+http://flowplayer.org/tools/
+
+## About this Fork ##
+
+Since JQuery tools refuses to get their code on Github, I took the liberty of maintaining a version. I'll be making my own bug fixes. Send me a github msg if they release a new version and I forget to update this fork.
+
+Honestly, I think this codebase is a jQuery coding disaster. The author has very little understanding of jQuery custom event mechanism, and decides to implement his own inner api object within each plugin. Makes it very hard to hack. 
+
+Eventually, I'd like to turn this fork into a "proper" implementation of these plugins. Would love any help on this, so please fork this repo and submit patches.
+
+## LICENSE ##
+
+Dual licensed under MIT and GPL 2+ licenses
+http://www.opensource.org/licenses
+
+## COPYRIGHT ##
+
+Copyright (c) 2009 Tero Piirainen
+http://flowplayer.org/tools/overlay.html#apple
+
--- tools.scrollable.mousewheel.js
+++ tools.scrollable.mousewheel.js
@@ -19,85 +19,85 @@
  * Created: 2008-07-01 | Updated: 2008-07-14
  */
 (function($) {
-		
-	$.fn.wheel = function( fn ){
-		return this[ fn ? "bind" : "trigger" ]( "wheel", fn );
-	};
+    
+  $.fn.wheel = function( fn ){
+    return this[ fn ? "bind" : "trigger" ]( "wheel", fn );
+  };
 
-	// special event config
-	$.event.special.wheel = {
-		setup: function(){
-			$.event.add( this, wheelEvents, wheelHandler, {} );
-		},
-		teardown: function(){
-			$.event.remove( this, wheelEvents, wheelHandler );
-		}
-	};
+  // special event config
+  $.event.special.wheel = {
+    setup: function(){
+      $.event.add( this, wheelEvents, wheelHandler, {} );
+    },
+    teardown: function(){
+      $.event.remove( this, wheelEvents, wheelHandler );
+    }
+  };
 
-	// events to bind ( browser sniffed... )
-	var wheelEvents = !$.browser.mozilla ? "mousewheel" : // IE, opera, safari
-		"DOMMouseScroll"+( $.browser.version<"1.9" ? " mousemove" : "" ); // firefox
+  // events to bind ( browser sniffed... )
+  var wheelEvents = !$.browser.mozilla ? "mousewheel" : // IE, opera, safari
+    "DOMMouseScroll"+( $.browser.version<"1.9" ? " mousemove" : "" ); // firefox
 
-	// shared event handler
-	function wheelHandler( event ) {
-		
-		switch ( event.type ){
-			
-			// FF2 has incorrect event positions
-			case "mousemove": 
-				return $.extend( event.data, { // store the correct properties
-					clientX: event.clientX, clientY: event.clientY,
-					pageX: event.pageX, pageY: event.pageY
-				});
-				
-			// firefox	
-			case "DOMMouseScroll": 
-				$.extend( event, event.data ); // fix event properties in FF2
-				event.delta = -event.detail / 3; // normalize delta
-				break;
-				
-			// IE, opera, safari	
-			case "mousewheel":				
-				event.delta = event.wheelDelta / 120;
-				break;
-		}
-		
-		event.type = "wheel"; // hijack the event	
-		return $.event.handle.call( this, event, event.delta );
-	}
-	
-	
-	// version number
-	var t = $.tools.scrollable; 
-	t.plugins = t.plugins || {};
-	t.plugins.mousewheel = {	
-		version: '1.0.1',
-		conf: { 
-			api: false,
-			speed: 50
-		} 
-	}; 
-	
-	// scrollable mousewheel implementation
-	$.fn.mousewheel = function(conf) {
+  // shared event handler
+  function wheelHandler( event ) {
+    
+    switch ( event.type ){
+      
+      // FF2 has incorrect event positions
+      case "mousemove": 
+        return $.extend( event.data, { // store the correct properties
+          clientX: event.clientX, clientY: event.clientY,
+          pageX: event.pageX, pageY: event.pageY
+        });
+        
+      // firefox  
+      case "DOMMouseScroll": 
+        $.extend( event, event.data ); // fix event properties in FF2
+        event.delta = -event.detail / 3; // normalize delta
+        break;
+        
+      // IE, opera, safari  
+      case "mousewheel":        
+        event.delta = event.wheelDelta / 120;
+        break;
+    }
+    
+    event.type = "wheel"; // hijack the event 
+    return $.event.handle.call( this, event, event.delta );
+  }
+  
+  
+  // version number
+  var t = $.tools.scrollable; 
+  t.plugins = t.plugins || {};
+  t.plugins.mousewheel = {  
+    version: '1.0.1',
+    conf: { 
+      api: false,
+      speed: 50
+    } 
+  }; 
+  
+  // scrollable mousewheel implementation
+  $.fn.mousewheel = function(conf) {
 
-		var globals = $.extend({}, t.plugins.mousewheel.conf), ret;
-		if (typeof conf == 'number') { conf = {speed: conf}; }
-		conf = $.extend(globals, conf);
-		
-		this.each(function() {		
+    var globals = $.extend({}, t.plugins.mousewheel.conf), ret;
+    if (typeof conf == 'number') { conf = {speed: conf}; }
+    conf = $.extend(globals, conf);
+    
+    this.each(function() {    
 
-			var api = $(this).scrollable();
-			if (api) { ret = api; }
-			
-			api.getRoot().wheel(function(e, delta)  { 
-				api.move(delta < 0 ? 1 : -1, conf.speed || 50);
-				return false;
-			});
-		});
-		
-		return conf.api ? ret : this;
-	};
-	
+      var api = $(this).scrollable();
+      if (api) { ret = api; }
+      
+      api.getRoot().wheel(function(e, delta)  { 
+        api.move(delta < 0 ? 1 : -1, conf.speed || 50);
+        return false;
+      });
+    });
+    
+    return conf.api ? ret : this;
+  };
+  
 })(jQuery); 
 
